8a1ab3778e82d9c29f8a7401c3455c3d559314b9
Andrew <chernigovsky108@gmail.com>
2021-06-16 16:10:56 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-import  {getRandomNumber1} from './utilits.js';
-import  {makeAuthor, makeOffer, makeLocation, makeFullOffer} from './data.js';
-
-getRandomNumber1();
-makeAuthor();
-makeOffer();
-makeLocation();
+import  {makeFullOffer} from './data.js';
 
 makeFullOffer();
+
