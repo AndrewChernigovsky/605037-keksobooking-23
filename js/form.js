cbebc78026445d076b294e46b9ad19953dc8a4be
Andrew <chernigovsky108@gmail.com>
2021-07-02 14:24:35 +0300

    form-validation

--- js/form.js
+++ js/form.js
@@ -4,6 +4,45 @@ const formFieldset = document.querySelectorAll('.ad-form__element');
 const formMap = document.querySelector('.map__filters');
 const formMapOption = document.querySelectorAll('.map__filter');
 
+const minPrice = {
+  flat: 1000,
+  bungalow: 0,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+const guestRoom = {
+  1: [2],
+  2: [1, 2],
+  3: [0, 1, 2],
+  100: [3],
+};
+
+const room = document.querySelector('#room_number');
+const guest = document.querySelector('#capacity');
+
+room.addEventListener('change', () => {
+
+  for (let i = 0; i < guest.options.length; i++) {
+    guest.options[i].disabled = true;
+  }
+
+  for (let i = 0; i < guestRoom[room.value].length; i++) {
+    guest.options[guestRoom[room.value][i]].disabled = false;
+  }
+
+});
+
+const typeHouseSelect = document.querySelector('#type');
+const housePrice = document.querySelector('#price');
+
+const onTypeChange = () => {
+  housePrice.placeholder = minPrice[typeHouseSelect.value];
+};
+
+typeHouseSelect.addEventListener('change', onTypeChange);
+
 const formSwitch = (toggle) => {
   const classFormDis = formDataFilter.classList.contains('ad-form--disabled');
   const classFormMapDis = formDataFilter.classList.contains('map__filters--disabled');
@@ -26,7 +65,5 @@ const formSwitch = (toggle) => {
     formFieldsetHeader.disabled = false;
   }
 };
-
-
 
 export { formSwitch };
