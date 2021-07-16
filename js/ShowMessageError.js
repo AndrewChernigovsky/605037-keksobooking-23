6ebed656040c480079d4ea67743b1c7dfb14ba6d
Andrew <chernigovsky108@gmail.com>
2021-07-16 13:08:02 +0300

    1

--- js/ShowMessageError.js
+++ js/ShowMessageError.js
@@ -1,4 +1,4 @@
-import {marker, defaultCoord, mapCanvas} from './map.js';
+import { marker, defaultCoord, mapCanvas } from './map.js';
 
 const bodyMain = document.querySelector('body');
 
@@ -26,24 +26,24 @@ const successMessage = function (message) {
 
 const errorMessage = (typeMessage) => {
     const templateModal = document.querySelector(`#error`)
-    .content
-    .querySelector(`.error`);
+        .content
+        .querySelector(`.error`);
     const modalElement = templateModal.cloneNode(true);
-    
+
     const onModalEscKeydown = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-    evt.preventDefault();
-    modalElement.remove();
-    document.removeEventListener('keydown', onModalEscKeydown);
-    }
+        if (evt.key === 'Escape' || evt.key === 'Esc') {
+            evt.preventDefault();
+            modalElement.remove();
+            document.removeEventListener('keydown', onModalEscKeydown);
+        }
     };
-    
+
     modalElement.addEventListener('click', () => {
-    modalElement.remove();
+        modalElement.remove();
     });
     document.addEventListener('keydown', onModalEscKeydown);
     bodyMain.appendChild(modalElement);
-    };
+};
 
 const closeMessage = function (e) {
     if (template) {
@@ -75,8 +75,8 @@ const resetFormData = () => {
     const selectCapacity = document.querySelector('#capacity');
     const formData = document.querySelector('.ad-form');
     formData.reset();
-    selectRooms.value = '1'; 
-    selectCapacity.value = '1'; 
+    selectRooms.value = '1';
+    selectCapacity.value = '1';
     inputAddress.value = `${defaultCoord.lat.toFixed(5)} , ${defaultCoord.lng.toFixed(5)}`;
     mapCanvas.closePopup();
     ;
@@ -84,8 +84,8 @@ const resetFormData = () => {
 
 const button = document.querySelector('.ad-form__reset');
 button.addEventListener('click', (evt) => {
-   evt.preventDefault();
-   resetFormData();
+    evt.preventDefault();
+    resetFormData();
 });
 
-export {successMessage, errorMessage, resetFormData};
+export { successMessage, errorMessage, resetFormData };
